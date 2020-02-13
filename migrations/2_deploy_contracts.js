08807d5ec87345b236577b83ff5981449df484d2
Bekket <jackatwork@yandex.ru>
2020-02-13 18:27:14 +0300

    truffle migrate bug fixed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 //var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var SuperFactory = artifacts.require("./SuperFactory.sol");
 var KNS = artifacts.require("./KNS.sol");
-var accounts = web3.eth.getAccounts();
+//var accounts = web3.eth.getAccounts();
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network, accounts) {
  // deployer.deploy(SimpleStorage);
  deployer.deploy(KNS).then(function() {
   console.log(accounts);
   console.log(accounts[0]);
-  return deployer.deploy(SuperFactory, KNS.address,accounts[0],accounts[0]);
+  return deployer.deploy(SuperFactory, KNS.address,accounts[1],accounts[2]);
 });
 };
