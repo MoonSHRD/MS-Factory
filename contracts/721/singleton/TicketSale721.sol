ff1a15b48129ff26bd6320c40b11f13916135eff
Bekket <jackatwork@yandex.ru>
2020-06-03 19:09:19 +0300

    scanned tickets counter and getter for it in TicketSale721

--- contracts/721/singleton/TicketSale721.sol
+++ contracts/721/singleton/TicketSale721.sol
@@ -5,7 +5,9 @@ import './Ticket721.sol';
 
 contract TicketSale721 is TokenSale721 {
 
+using Counters for Counters.Counter;
 
+Counters.Counter scanned_tickets;
 
 constructor(uint256 rate, address payable wallet, Ticket721 token, uint sale_limit, string memory jid) TokenSale721(rate,wallet,token, sale_limit, jid) public {}
 
@@ -44,8 +46,13 @@ function redeemTicket(address visitor, uint256 token_id) public {
     Ticket721 ticket_token = super.token();
     uint256 event_id = super.event_id();
     ticket_token.redeemTicket(visitor, token_id, event_id);
+    scanned_tickets.increment();
 }
 
+function getScannedTicketsCount() public view  returns(uint)  {
+    uint st = scanned_tickets.current();
+    return st;
+}
 
 
 }
