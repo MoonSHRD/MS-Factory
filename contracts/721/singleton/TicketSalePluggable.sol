a66aca94cadace5bd5088c680c1b755045729002
JackBekket <jackatwork@yandex.ru>
2020-06-03 19:14:10 +0300

    Enchansment/fees and sold count (#79)
    * scanned tickets counter and getter for it in TicketSale721
    
    * scanned tickets counter at TicketSalePluggable

--- contracts/721/singleton/TicketSalePluggable.sol
+++ contracts/721/singleton/TicketSalePluggable.sol
@@ -6,6 +6,8 @@ import './Ticket721.sol';
 contract TicketSalePluggable is PluggableSale {
 
 
+using Counters for Counters.Counter;
+Counters.Counter scanned_tickets;
 
 constructor(uint256 rate, address payable _origin, uint sale_limit) PluggableSale(rate, _origin, sale_limit) public {}
 
@@ -44,8 +46,12 @@ function redeemTicket(address visitor, uint256 token_id) public {
     Ticket721 ticket_token = super.token();
     uint256 event_id = super.event_id();
     ticket_token.redeemTicket(visitor, token_id, event_id);
+    scanned_tickets.increment();
 }
 
-
+function getScannedTicketsCount() public view  returns(uint)  {
+    uint st = scanned_tickets.current();
+    return st;
+}
 
 }
